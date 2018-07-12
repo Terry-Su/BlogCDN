9095643148facb68f8bd75dd9b9d82f34d6acdfa
TerrySu <2684759855@qq.com>
2018-07-12 23:10:20 +0800

    [Iframes-css-margin-collapsing] Add parent and first/last child

---
+++ server.js
@@ -0,0 +1,29 @@
+const browserSync = require( 'browser-sync' )
+
+const bs = browserSync.create()
+const serverPath = __dirname
+
+const serverconfig = {
+	server: {
+		baseDir: serverPath,
+		directory: true,
+	},
+	files: [
+		`${serverPath}/**`
+	],
+	// port: 8600,
+	open: false,
+}
+
+
+function init() {
+	try {
+		bs.init( serverconfig )
+	} catch( e ) {
+		init()
+	}
+}
+
+init()
+
+
