89d335d359c75948ee650ebe720482b8ab4fa7a3
TerrySu <2684759855@qq.com>
2018-09-19 22:28:16 +0800

    update

--- iframes/js/debounce/search/index.js
+++ iframes/js/debounce/search/index.js
@@ -1,15 +1,23 @@
-const btnA = document.getElementById('btnA')
-const btnB = document.getElementById('btnB')
+// Original search button, without being debounced 
 const originalBtn = document.getElementById('originalBtn')
+
+// Search button using debounce mode 1
+const btnA = document.getElementById('btnA')
+
+// Search button using debounce mode 2
+const btnB = document.getElementById('btnB')
+
 const logContainer = document.getElementById('logContainer')
 let count = 0
 
+
+originalBtn.onclick = log
 btnA.onclick =  _.debounce( log, 1000 )
 btnB.onclick =  _.debounce( log, 1000, {
   leading: true,
   trailing: false
 } )
-originalBtn.onclick = log
+
 
 function log() {
   count = count + 1
