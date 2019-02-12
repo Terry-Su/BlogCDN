cf6b59b81f2c452fc1956089a0fbfcccfe7992cf
TerrySu <2684759855@qq.com>
2019-02-12 16:35:19 +0800

    update

--- iframes/css/flex/comon-layouts/responsive/index.js
+++ iframes/css/flex/comon-layouts/responsive/index.js
@@ -3,10 +3,10 @@ const el = document.getElementsByClassName( 'container' )[ 0 ]
 let count = 0
 const animate  = function() {
   if ( count > 100 ) {
-    count = -0.5
+    count = -0.3
   } 
 
-  count += 0.5
+  count += 0.3
 
   el.style.width = count + '%'
   console.log( 123 )
